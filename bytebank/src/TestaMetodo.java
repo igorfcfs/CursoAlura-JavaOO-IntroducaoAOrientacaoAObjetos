463eb0afdcb08a1a66b3558178c31ef6da9125a4
igorfcfs <igorfcfs@gmail.com>
2022-02-16 16:28:45 -0300

    Definindo Comportamento

---
+++ bytebank/src/TestaMetodo.java
@@ -0,0 +1,25 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaDoPaulo/*conta*/ = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		boolean sucessoTransferencia = contaDaMarcela.tranfere(300, contaDoPaulo);
+		if (sucessoTransferencia) {
+			System.out.println("Transferencia feita com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
+	}
+}
