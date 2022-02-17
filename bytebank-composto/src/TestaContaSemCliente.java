1e9e7271ffc3b21ec7cdceb500e159725236ac8f
igorfcfs <igorfcfs@gmail.com>
2022-02-17 16:28:59 -0300

    Encapsulamento e Visibilidade

--- bytebank-composto/src/TestaContaSemCliente.java
+++ bytebank-composto/src/TestaContaSemCliente.java
@@ -3,7 +3,8 @@ public class TestaContaSemCliente {
 
 	public static void main(String[] args) {
 		Conta contaDaMarcela = new Conta();
-		System.out.println(contaDaMarcela.saldo);
+		// System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDaMarcela.getSaldo());
 		
 		System.out.println(contaDaMarcela.titular);
 		contaDaMarcela.titular = new Cliente();
